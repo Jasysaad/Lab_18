2a59e2d1571f0382bdb24f2bc0da8d27bef07df6
jasmine saad <jasmineysaad@gmail.com>
2021-03-04 21:55:43 -0500

    Add project files.

---
+++ lab18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab18
+{
+    class Program
+ {
+
+        static void Main(string[] args)
+        {
+            var list = new List{11};
+
+
+        }
+    }
+}
